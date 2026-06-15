d9f87579e1c81636883a8b5f7ef95f580fae5774
duffc25-lab <duffc25@gmail.com>
2026-06-15 13:13:00 -0700

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,11 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    return {
+        "anger": 0.01,
+        "disgust": 0.02,
+        "fear": 0.03,
+        "joy": 0.90,
+        "sadness": 0.04,
+        "dominant_emotion": "joy"
+    }
